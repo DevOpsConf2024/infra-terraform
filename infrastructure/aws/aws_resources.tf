d9411851d52ab947e5a3bf95919c7fafe64c30ff
Giorgio Lasala <salem84@users.noreply.github.com>
2024-04-17 17:30:37 +0200

    sample

---
+++ infrastructure/aws/aws_resources.tf
@@ -0,0 +1,52 @@
+# IAM Requirements : S3
+# - CreateBucket
+# - PutBucketPublicAccessBlock
+# - PutBucketEncryption
+# - PutBucketVersioning
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-k8-tf-state"
+ 
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# IAM Requirements : Dynamo
+# - DescribeTable
+# - CreateTable
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "my-k8-tf-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
